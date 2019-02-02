d3a48ea6269049d12985228239558049c83f46ce
wujw <wujiawei@unicde.com>
2019-02-02 13:01:19 +0800

    docker-compose.yml

--- rabbitMQ_Customer/src/main/java/rabbitMq/Run.java
+++ rabbitMQ_Customer/src/main/java/rabbitMq/Run.java
@@ -12,26 +12,41 @@ import rabbitMq.Customer;
 import tool.IniRead;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static java.lang.Thread.sleep;
+
 @Slf4j
 public class Run {
 
     public static void main(String[] args) {
         System.out.println("main Start");
-        addLogConfig();
-        log.info("main Start");
+        //addLogConfig();
+        //log.info("main Start");
 
         try {
-            Customer.Run();
+           //Customer.Run();
 
         }
         catch (Exception ex)
         {
-            log.error( ex.getMessage());
+            //log.error( ex.getMessage());
         }
-//        while(true)
-//        {
-//            int a=1;
-//        }
+        while(true)
+        {
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+            String T=df.format(new Date());// new Date()为获取当前系统时间
+            System.out.println(T);
+           //log.info(T);
+           try {
+               sleep(5000);
+           }
+           catch (Exception ex)
+           {
+               //log.error( ex.getMessage());
+           }
+        }
         //log.info("main End");
         //System.out.println("main End");
     }
